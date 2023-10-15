af6533147b2038a2186a0798eee29d2c48078583
linnaa-a <lina.aderkichi@epsi.fr>
2023-10-15 18:57:11 +0200

    Potrfolio Lina Aderkichi

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+let box = document.querySelector('.Box_mystery');
+    let hiddenText = document.querySelector('.hidden-text');
+    let img = document.querySelector('.Box_mystery img:nth-child(1)');
+    let img2 = document.querySelector('.Box_mystery img:nth-child(2)');
+    box.addEventListener('click', function () {
+        img.classList.add('Up');
+        hiddenText.classList.add('Up-text');
+        img.classList.add('hidden');
+        img2.classList.add('Up-down');
+        setTimeout(function () {
+            img.classList.remove('Up');
+        }, 1000)
+    })
